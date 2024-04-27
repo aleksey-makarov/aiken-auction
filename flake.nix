b4679de91e11b7cf7bc17540eb2ed426eb065ad0
Aleksei Makarov <aleksey.makarov@gmail.com>
2024-04-27 19:43:50 +0200

    Start with nix

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "aiken-auction";
+
+  nixConfig.bash-prompt = "aiken-auction";
+  nixConfig.bash-prompt-prefix = "[\\033[1;33m";
+  nixConfig.bash-prompt-suffix = "\\033[0m \\w]$ ";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-vscode-extensions = {
+      url = "github:nix-community/nix-vscode-extensions";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    nix-vscode-extensions,
+  }: let
+    system = "x86_64-linux";
+
+    pkgs = nixpkgs.legacyPackages.${system};
+
+    extensions = nix-vscode-extensions.extensions.${system};
+
+    vscode = pkgs.vscode-with-extensions.override {
+      vscode = pkgs.vscodium;
+      vscodeExtensions = [
+        extensions.vscode-marketplace.bbenoist.nix
+        # extensions.vscode-marketplace.TxPipe.aiken
+        # extensions.vscode-marketplace.github.vscode-github-actions
+        # extensions.vscode-marketplace.ms-vscode.cpptools
+        # extensions.vscode-marketplace.haskell.haskell
+        # extensions.vscode-marketplace.justusadam.language-haskell
+      ];
+    };
+  in {
+
+    devShells.${system} = rec {
+      aiken-auction = with pkgs;
+        mkShell {
+          packages = [vscode];
+          # inputsFrom = [pkgs.virtio-target];
+          shellHook = ''
+            export HOME=$(pwd)
+            # echo "Hello world"
+          '';
+        };
+      default = aiken-auction;
+    };
+  };
+}
