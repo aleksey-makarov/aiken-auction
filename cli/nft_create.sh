4afcdddda06a13198fdabe42d50ce764c8781121
Aleksei Makarov <aleksey.makarov@gmail.com>
2024-06-21 16:36:44 +0200

    nft_create.sh: Remove dead code

--- cli/nft_create.sh
+++ cli/nft_create.sh
@@ -52,36 +52,3 @@ EOF
 
 echo "$tokenname_hex" > nft_name_hex.txt
 echo "$policy_id" > nft_policy_id.txt
-
-exit
-
-set -x
-
-cardano-cli latest transaction build \
-    --testnet-magic 2 \
-    --out-file "$tx_unsigned_name" \
-    --tx-in $utxo \
-    --change-address "$wallet_address" \
-    --mint "1 $policy_id.$tokenname_hex" \
-    --mint-script-file "$policy_script_name" \
-    --required-signer-hash "$policy_key_hash" \
-    --metadata-json-file "$methadata_file_name" \
-    --tx-out "$(wallets_get_address wallet_nft)+1193870+1 ${policy_id}.${tokenname_hex}" \
-
-#  --tx-out "$my_nami_address"+"1202490"+"1 $policyid.$tokenname1" \
-#  --tx-in 'c5ba91bf3a84e71fe8d16a3edaacf9466be598174653a4404b5471b8f84fccb3#0' \
-#  --tx-out "$roberto_address"+"10000000"+"12 $policyid.$tokenname1" \
-
-cardano-cli latest transaction sign \
-    --testnet-magic 2 \
-    --tx-body-file "$tx_unsigned_name" \
-    --out-file "$tx_signed_name" \
-    --signing-key-file "$policy_signing_key_file" \
-    --signing-key-file "$utxo_signing_key_file" \
-
-cardano-cli latest transaction submit \
-    --testnet-magic 2 \
-    --tx-file "$tx_signed_name"
-
-rm "$tx_unsigned_name"
-rm "$tx_signed_name"
